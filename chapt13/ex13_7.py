abc9b5d57a5e425d68e678044049b2b402123f0f
Simon Holloway <52679357+Simon-Holloway@users.noreply.github.com>
2021-01-12 17:02:01 -0600

    Add files via upload

--- chapt13/ex13_7.py
+++ chapt13/ex13_7.py
@@ -2,18 +2,28 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
 
+url =  'http://py4e-data.dr-chuck.net/known_by_Talha.html'
+count = int(input('Enter count \n'))
+enpos = int(input('Enter position \n'))
+i = 0
+
+
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-#url = input('Enter - ')
-html = urllib.request.urlopen('http://py4e-data.dr-chuck.net/known_by_Fikret.html', context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
 
-# Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    ntag = tag.get('href', None)
-    ntag.split('_')
-    print(ntag)
+while i < count:
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    pos = 0
+    for tag in tags:
+        ntag = tag.get('href', None)
+        pos += 1
+        if pos == enpos:
+            url=ntag
+            print(ntag)
+            break
+    i += 1
